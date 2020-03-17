d7ff7bf81384f517c97a00ad9fb663de0e12c777
Luca <luca-marconi@hotmail.com>
2020-03-17 10:43:34 +0100

    aggiunta "già cliccato"

--- js/main.js
+++ js/main.js
@@ -1,24 +1,29 @@
     $('.square').click(function() {
         var clicked = $(this);
-        $.ajax({
-            url: 'https://flynn.boolean.careers/exercises/api/random/int',         // inserire url api
-            method: 'GET',
-            success: function(data) {
-                var numeroRandom = data.response;
-                console.log(numeroRandom);
-                    myFunction(numeroRandom, clicked)
-            },
-            error: function() {
-            }
-        });
+        if ((clicked.hasClass('yellow')) || (clicked.hasClass('green'))) {
+            alert('hai già cliccato questo quadrato');
+        } else {
+            $.ajax({
+                url: 'https://flynn.boolean.careers/exercises/api/random/int',         // inserire url api
+                method: 'GET',
+                success: function(data) {
+                    var numeroRandom = data.response;
+                    console.log(numeroRandom);
+                        myFunction(numeroRandom, clicked)
+                },
+                error: function() {
+                }
+            });
+        }
+
     });
 
     function myFunction(numeroRandom, clicked) {
         if (numeroRandom <= 5) {
-            $(clicked).addClass('yellow');
+            $(clicked).addClass('yellow').removeClass('green');
             $(clicked).text(numeroRandom);
         } else {
-            $(clicked).addClass('green');
+            $(clicked).addClass('green').removeClass('yellow');
             $(clicked).text(numeroRandom);
         }
     }
