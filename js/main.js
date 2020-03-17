bae9de545e805bb4f53722e990eca31cf8298ff8
Luca <luca-marconi@hotmail.com>
2020-03-17 09:10:41 +0100

    modifica funzione

--- js/main.js
+++ js/main.js
@@ -6,15 +6,19 @@
             success: function(data) {
                 var numeroRandom = data.response;
                 console.log(numeroRandom);
-                    if (numeroRandom <= 5) {
-                        $(clicked).addClass('yellow');
-                        $(clicked).text(numeroRandom);
-                    } else {
-                        $(clicked).addClass('green');
-                        $(clicked).text(numeroRandom);
-                    }
+                    myFunction(numeroRandom, clicked)
             },
             error: function() {
             }
         });
     });
+
+    function myFunction(numeroRandom, clicked) {
+        if (numeroRandom <= 5) {
+            $(clicked).addClass('yellow');
+            $(clicked).text(numeroRandom);
+        } else {
+            $(clicked).addClass('green');
+            $(clicked).text(numeroRandom);
+        }
+    }
